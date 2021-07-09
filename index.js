db26202938493e92c0c4dfdc4024d05458d43b1f
TeoChiriac <56512422+TeoChiriac@users.noreply.github.com>
2021-07-09 22:34:49 +0300

    Index.js
    recruitee widget

---
+++ index.js
@@ -0,0 +1,29 @@
+
+      <div id="recruitee-careers"></div>
+      <script type="text/javascript" src="https://d10zminp1cyta8.cloudfront.net/widget.js"></script>
+      <script type="text/javascript">
+        var widget = new RTWidget({ 
+ "companies": [
+   63735
+ ],
+ "detailsMode": "popup",
+ "language": "en",
+ "departmentsFilter": [],
+ "themeVars": {
+   "primary": "#1999e3",
+   "secondary": "#000",
+   "text": "#5c6f78",
+   "textDark": "#37474f",
+   "fontFamily": "\"Helvetica Neue\", Helvetica, Arial, \"Lucida Grande\", sans-serif;",
+   "baseFontSize": "16px"
+ },
+ "flags": {
+   "showLocation": true,
+   "showCountry": true,
+   "showCity": true,
+   "groupByLocation": false,
+   "groupByDepartment": false,
+   "groupByCompany": false
+ }
+ });
+      </script>
